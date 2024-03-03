532c5f1982a52e21c7f9f3518c5ccb9cd1b03bdb
karanjakhar <karanjakhar49@gmail.com>
2024-03-03 23:12:32 +0530

    vae decoder code added

---
+++ stable_diffusion/decoder.py
@@ -0,0 +1,72 @@
+import torch 
+from torch import nn 
+from torch.nn import functional as F 
+from attention import SelfAttention 
+
+
+class VAE_ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels) -> None:
+        super().__init__()
+        self.groupnorm_1 = nn.GroupNorm(32, in_channels)
+        self.conv_1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+
+        self.groupnorm_2 = nn.GroupNorm(32, out_channels)
+        self.conv_2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
+
+        if in_channels == out_channels:
+            self.residual_layer = nn.Identity()
+        else:
+            self.residual_layer = nn.Conv2d(in_channels, out_channels, kernel_size=1, padding=0)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # x: (Batch_Size, in_channels, height, width)
+
+        residue = x 
+        x = self.groupnorm_1(x)
+
+        x = F.silu(x)
+
+        x = self.conv_1(x)
+
+        x = self.groupnorm_2(x)
+
+        x = F.silu(x) 
+        
+        x = self.conv_2(x)
+
+
+        return x + self.residual_layer(residue)
+    
+
+class VAE_AttentionBlock(nn.Module):
+    def __init__(self, channels) -> None:
+        super().__init__()
+        self.groupnorm_1 = nn.GroupNorm(32, channels)
+        self.attention = SelfAttention(1, channels)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # x: (Batch_Size, features, height, width)
+
+        residue = x 
+
+        n, c, h, w = x.shape 
+
+        # (Batch_Size, features, height, width) -> (Batch_Size, features, height * width)
+
+        x = x.view(n, c, h * w)
+
+        # (Batch_Size, features, height * width) -> (Batch_Size, height * width, features)
+        x = x.transpose(-1, -2)
+
+        x = self.attention(x)
+
+        # (batch_size, height * width, features) -> (Batch_Size, features, height * width)
+        x = x.transpose(-1, -2)
+
+        # (Batch_Size, features, height * width) -> (Batch_Size, features, height, width)
+        x = x.view(n,c,h,w)
+
+        x += residue
+
+        return x 
+
